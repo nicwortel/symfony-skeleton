df7af140db59055e48810dafcb0a321106a34842
Nic Wortel <nic@nicwortel.nl>
2017-10-04 15:49:29 +0200

    Enable Twig

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
